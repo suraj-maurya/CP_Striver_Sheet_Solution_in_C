9890bd518b36aa21b5d83cd3042ac717a0b33d1b
Suraj Maurya <mnaveen1888@gmail.com>
2024-01-25 17:10:29 +0530

    Floor Number

--- Maths/Q4_FloorNumber.c
+++ Maths/Q4_FloorNumber.c
@@ -2,10 +2,22 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
-    int n;
-    scanf("%d ", &n);
-    
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n, x, floor = 1;
+        scanf("%d%d", &n, &x);
+        if (n > 2)
+        {
+            floor = ((n - 2) / x) + 1;
+            if (((n - 2) % x) != 0) floor += 1;
+        }
+
+        printf("%d\n", floor);
+    }
 
     return 0;
 }
